eac2ca47aad858e575a8719c16d8d32d6136a4e0
Mr otmane <otmaneanna2020@gmail.com>
2025-07-17 20:48:53 +0100

    replace class tags inside on className

--- src/App.jsx
+++ src/App.jsx
@@ -29,28 +29,28 @@ const App = () => {
       <button type="button" className="btn btn-link">
         Link
       </button>
-      <div class="dropdown">
+      <div className="dropdown">
         <button
-          class="btn btn-secondary dropdown-toggle"
+          className="btn btn-secondary dropdown-toggle"
           type="button"
           data-bs-toggle="dropdown"
           aria-expanded="false"
         >
           Dropdown button
         </button>
-        <ul class="dropdown-menu">
+        <ul className="dropdown-menu">
           <li>
-            <a class="dropdown-item" href="#">
+            <a className="dropdown-item" href="#">
               Action
             </a>
           </li>
           <li>
-            <a class="dropdown-item" href="#">
+            <a className="dropdown-item" href="#">
               Another action
             </a>
           </li>
           <li>
-            <a class="dropdown-item" href="#">
+            <a className="dropdown-item" href="#">
               Something else here
             </a>
           </li>
