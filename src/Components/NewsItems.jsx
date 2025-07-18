a450e429f217a1ed0f80ee3da8d6cac57e277c84
Mr otmane <otmaneanna2020@gmail.com>
2025-07-18 19:21:04 +0100

    make dark mode for this classname

--- src/Components/NewsItems.jsx
+++ src/Components/NewsItems.jsx
@@ -1,9 +1,18 @@
-
-
-const NewsItems = () => {
+const NewsItems = (title, description, src, url) => {
   return (
-    <div></div>
-  )
-}
+    <div className="card bg-dark text-light" style={{maxWidth:"345px"}}>
+      <img src= {src} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">
+          {description}
+        </p>
+        <a href={url} className="btn btn-primary">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default NewsItems
+export default NewsItems;
