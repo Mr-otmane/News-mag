726db310940933797f73bf68a21ba600c4e1e816
Mr otmane <otmaneanna2020@gmail.com>
2025-07-18 17:59:53 +0100

    remove import react js

--- src/Components/NewsItems.jsx
+++ src/Components/NewsItems.jsx
@@ -1,14 +1,9 @@
-export const NewsItems = ({ title, description, src, url }) => {
+
+
+const NewsItems = () => {
   return (
-    <div className="card" style={{ maxWidth: "345px" }}>
-      <img src={src} className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
-        <a href={url} className="btn btn-primary">
-          Read More
-        </a>
-      </div>
-    </div>
-  );
-};
+    <div></div>
+  )
+}
+
+export default NewsItems
