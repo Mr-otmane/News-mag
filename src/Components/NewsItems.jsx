a21ed6501b33dbb2ee555c484f1ceae4ead548b1
Mr otmane <otmaneanna2020@gmail.com>
2025-07-18 19:21:54 +0100

    add margin bottom: 3 add this file

--- src/Components/NewsItems.jsx
+++ src/Components/NewsItems.jsx
@@ -1,6 +1,6 @@
 const NewsItems = (title, description, src, url) => {
   return (
-    <div className="card bg-dark text-light" style={{maxWidth:"345px"}}>
+    <div className="card bg-dark text-light mb-3" style={{maxWidth:"345px"}}>
       <img src= {src} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
