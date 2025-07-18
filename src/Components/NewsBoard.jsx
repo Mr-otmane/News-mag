1c09d4c5fca77d578293e6e4576e2205cdd7df19
Mr otmane <otmaneanna2020@gmail.com>
2025-07-18 11:00:59 +0100

    add new url inside this file

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 
 
 export const NewsBoard = () => {
-    const
+    const [articles,setArticles] = useState([]);
+
+    useEffect(()=>{
+        let url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=24bf473e4e654f85a4df74c7499235a4`;
+    })
+
   return (
     <div>
         <h2 className="text-center">Latest <span className="badge bg-danger"> News</span></h2>
