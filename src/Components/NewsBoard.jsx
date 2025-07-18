1b997683a336d8979b265805bca1ff2ad783b665
Mr otmane <otmaneanna2020@gmail.com>
2025-07-18 11:05:31 +0100

    Import key inside this file

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -5,7 +5,7 @@ export const NewsBoard = () => {
     const [articles,setArticles] = useState([]);
 
     useEffect(()=>{
-        let url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=24bf473e4e654f85a4df74c7499235a4`;
+        let url =`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
     })
 
   return (
