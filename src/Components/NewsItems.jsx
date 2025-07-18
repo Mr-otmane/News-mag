86ccb4ec05c1b5475c8c871b5875784c1cd233c7
Mr otmane <otmaneanna2020@gmail.com>
2025-07-18 19:26:30 +0100

    put slice methode in this file

--- src/Components/NewsItems.jsx
+++ src/Components/NewsItems.jsx
@@ -4,9 +4,14 @@ const NewsItems = (title, description, src, url) => {
       className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2"
       style={{ maxWidth: "345px" }}
     >
-      <img src={src} style={} className="card-img-top" alt="..." />
+      <img
+        src={src}
+        style={{ height: "200px", width: "360px" }}
+        className="card-img-top"
+        alt="..."
+      />
       <div className="card-body">
-        <h5 className="card-title">{title}</h5>
+        <h5 className="card-title">{title.slice()}</h5>
         <p className="card-text">{description}</p>
         <a href={url} className="btn btn-primary">
           Read More
