97f096860a494ab9743d703510054b70182dfe1b
Mr otmane <otmaneanna2020@gmail.com>
2025-07-18 11:24:18 +0100

    put title and src and description inside this file

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -17,6 +17,9 @@ export const NewsBoard = () => {
       <h2 className="text-center">
         Latest <span className="badge bg-danger"> News</span>
       </h2>
+      {articles.map((news,index)={
+        return <NewsItems key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+      })}
     </div>
   );
 };
--- src/Components/NewsItems.jsx
+++ src/Components/NewsItems.jsx
@@ -1,14 +1,14 @@
-
-
-export const NewsItems = () => {
+export const NewsItems = ({ title, description, src, url }) => {
   return (
     <div className="card" style="width: 18rem;">
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-  )
-}
+      <img src={src} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <a href={url} className="btn btn-primary">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
