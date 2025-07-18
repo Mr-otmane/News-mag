74a1ef808816e5740a0766cb413de1989392632d
Mr otmane <otmaneanna2020@gmail.com>
2025-07-18 10:51:46 +0100

    make the bg - red in this classname

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -3,7 +3,7 @@
 export const NewsBoard = () => {
   return (
     <div>
-        <h2 className="text-center">Latest <span className=""> News</span></h2>
+        <h2 className="text-center">Latest <span className="badge bg-danger"> News</span></h2>
     </div>
   )
 }
