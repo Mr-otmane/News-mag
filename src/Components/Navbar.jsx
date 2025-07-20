4b0b7f159ed2d3bb8d446a09abf3f79a49fb7007
Mr otmane <otmaneanna2020@gmail.com>
2025-07-20 19:23:32 +0100

    remove home link in navbar likns

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -21,11 +21,8 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
+           
+             
             <li className="nav-item">
               <a className="nav-link" href="#">
                 Features
