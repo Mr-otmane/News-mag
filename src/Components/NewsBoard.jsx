7cf0b9070c4a11b772772db87f4afbfeaa35bfde
Mr otmane <otmaneanna2020@gmail.com>
2025-07-20 19:18:23 +0100

    put category element inside Url

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -6,7 +6,7 @@ const NewsBoard = ({category}) => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
       import.meta.env().VITE_API_KEY
     }`;
     fetch(url)
